11e6b0524f4fa4cae7425c63c0c2635d25cd8cc1
amankusumbe9 <53687104+amankusumbe9@users.noreply.github.com>
2023-04-18 15:55:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My parents new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie ')
-streamlit.text('Hard-Boiled, Free-Ranged egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie ')
+streamlit.text('🐔 Hard-Boiled, Free-Ranged egg')
+stremlit.text('🥑🍞 Avocado toast')
 
